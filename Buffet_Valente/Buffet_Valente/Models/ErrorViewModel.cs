5e992403d7d7c79876da2fe7906774767a824f8a
lucianovalente <luciano.valente@viannasempre.com.br>
2021-03-26 21:27:59 -0300

    Subindo projeoto inicial, páginas externas

---
+++ Buffet_Valente/Buffet_Valente/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Buffet_Valente.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
